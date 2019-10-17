fc8bd7411a276d7e028898f1f8c7ea2cb4b31075
phueb <info@philhuebner.com>
2019-10-17 11:37:02 -0500

    do not keep all w2dfs in memory - load them in once for making w2cf, and again for making term-by-doc mat

--- wikineighbors/builder.py
+++ wikineighbors/builder.py
@@ -4,6 +4,7 @@ import pickle
 from scipy import sparse
 from scipy.sparse import linalg as slinalg
 from collections import Counter
+from multiprocessing import Pool
 from cached_property import cached_property
 
 from wikineighbors.exceptions import WikiNeighborsNoMemory
@@ -30,21 +31,41 @@ class SimMatBuilder:
         vocab, sim_mat = self._build()
         self._save_to_disk(vocab, sim_mat)
         del vocab
-        del sim_mat  # TODO how to make sure it is deleted - weakref?
+        del sim_mat
 
     @staticmethod
-    def make_sim_mat(w2dfs, vocab, init_mat):  # this is slow
-        print('Making term-by-doc matrix...')
-        for col_id, w2df in enumerate(w2dfs):
-            try:
-                init_mat[:, col_id] = [w2df[w] for w in vocab]
-            except IndexError:
-                print(col_id)
+    def _make_term_by_window_mat_chunk(w2dfs_path, vocab):
+        print('Starting worker', flush=True)
 
+        with w2dfs_path.open('rb') as f:
+            w2dfs = pickle.load(f)
+        print('Worker loaded {}'.format(w2dfs_path))
+
+        chunk = np.zeros((len(vocab), len(w2dfs)), dtype=np.int16)
+        for col_id, w2df in enumerate(w2dfs):
+            chunk[:, col_id] = [w2df[w] for w in vocab]
+        print('Worker made matrix with shape {}'.format(chunk.shape), flush=True)
+        return chunk
+
+    def _make_term_by_doc_mat(self, vocab):
+        self.check_memory()
+
+        print('Making term-by-doc matrix...')
+        num_workers = 2  # using 6 may cause memory error
+        pool = Pool(num_workers)
+        chunks = pool.starmap(self._make_term_by_window_mat_chunk,
+                              zip(self.w2dfs_paths, vocab * len(self.w2dfs_paths)))
+
+        res = np.hstack(chunks, dtype=np.int16)  # TODO test
+        print('Term-by-doc matrix has shape {}'.format(res.shape))
+        return res
+
+    @staticmethod
+    def _make_sim_mat(term_doc_mat):
         # convert to sparse format
-        num_nonzeros = np.count_nonzero(init_mat)
-        print('Percentage of non-zeros in term-by-doc matrix: {}%'.format(num_nonzeros / init_mat.size * 100))
-        sparse_mat = sparse.csr_matrix(init_mat).asfptype()
+        num_nonzeros = np.count_nonzero(term_doc_mat)
+        print('Percentage of non-zeros in term-by-doc matrix: {}%'.format(num_nonzeros / term_doc_mat.size * 100))
+        sparse_mat = sparse.csr_matrix(term_doc_mat).asfptype()
 
         # svd
         u, s, v = slinalg.svds(sparse_mat, k=config.Sims.num_svd_dimensions)
@@ -55,8 +76,9 @@ class SimMatBuilder:
 
         return res
 
-    def make_vocab(self, w2cf):
-        print('Making vocab...')
+    def _make_vocab(self, w2cf):
+        print('Making vocab with size={} and cat={}'.format(
+            self.specs.vocab_size, self.specs.cat))
         vocab = set()
         num_too_big = 0
         for w, f in sorted(w2cf.items(), key=lambda i: i[1], reverse=True):
@@ -91,15 +113,6 @@ class SimMatBuilder:
         return res
 
     def _build(self):
-        # check that memory is sufficient
-        shape = (self.specs.vocab_size, self.specs.corpus_size)
-        try:
-            init_mat = np.zeros(shape, dtype=np.int16)
-            num_mbs = init_mat.nbytes / 1e6
-            print('Successfully initialized matrix with shape={} requiring {} megabytes'.format(shape, num_mbs))
-        except MemoryError:
-            raise WikiNeighborsNoMemory(shape)
-
         # make w2cf (word 2 corpus-frequency)
         w2cf = Counter()
         n = 0
@@ -110,17 +123,32 @@ class SimMatBuilder:
             for w2df in w2dfs:
                 w2cf.update(w2df)
                 n += 1
+            del w2dfs  # otherwise twice as much memory is used
         print('Loaded {} w2dfs from disk'.format(n))
 
         # make vocab
-        print('Making vocab with size={} and cat={}'.format(self.specs.vocab_size, self.specs.cat))
-        vocab = self.make_vocab(w2cf)
+        vocab = self._make_vocab(w2cf)
         del w2cf
 
+        # make term-doc mat
+        term_by_doc_mat = self._make_term_by_doc_mat(vocab)
+
         # make sim mat
-        sim_mat = self.make_sim_mat(w2dfs, vocab, init_mat)  # TODO in a multiprocesing loop, populate term-doc-mat
+        sim_mat = self._make_sim_mat(term_by_doc_mat)
 
         return vocab, sim_mat
+
+    def check_memory(self):
+        # check that memory is sufficient
+        shape = (self.specs.vocab_size, self.specs.corpus_size)
+        try:
+            init_mat = np.zeros(shape, dtype=np.int16)
+            num_mbs = init_mat.nbytes / 1e6
+            print('Successfully initialized matrix with shape={} requiring {} megabytes'.format(shape, num_mbs))
+        except MemoryError:
+            raise WikiNeighborsNoMemory(shape)
+        else:
+            del init_mat
 
     def _save_to_disk(self, vocab, sim_mat):
         # make dir + save to disk
